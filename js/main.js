4ff3efc2d56e6a992db3e6ebb4b9a88b97232891
tmillot08 <thomas.millot08@gmail.com>
2018-06-10 23:46:14 +0200

    azrara

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+function initMap() {
+        var cci = {lat: 49.768234, lng: 4.722480};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: cci
+        });
+        var image = "./img/simplon.png";
+        var marker = new google.maps.Marker({
+          position: cci,
+          title:"CCI des ardennes",
+          icon: image,
+          map: map
+        });
+      }
